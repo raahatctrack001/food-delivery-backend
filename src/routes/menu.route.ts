778468976545e17598c92a27ec10a46f61234c5e
Raahat Khan <raahatctrack001.rc@gmail.com>
2025-03-23 20:05:02 +0530

    debug controller import for menu

--- src/routes/menu.route.ts
+++ src/routes/menu.route.ts
@@ -1,7 +1,8 @@
 import express from "express" 
 import upload from "../middlewares/multer";
 import {isAuthenticated} from "../middlewares/isAuthenticated";
-import { addMenu, editMenu } from "../controller/menu.controller";
+import { addMenu, editMenu } from "../controllers/menu.controllers";
+
 
 const router = express.Router();
 
