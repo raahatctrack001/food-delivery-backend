3eccb84eb4993579699ae83737bcdf224d85a991
Raahat Khan <raahatctrack001.rc@gmail.com>
2025-03-23 20:00:59 +0530

    add all route related to restaurant

---
+++ src/routes/restaurant.route.ts
@@ -0,0 +1,18 @@
+import express from "express"
+import { isAuthenticated } from "../middlewares/isAuthenticated";
+import { createRestaurant, getRestaurant, getRestaurantOrder, getSingleRestaurant, searchRestaurant, updateOrderStatus, updateRestaurant } from "../controllers/restaurant.controllers";
+import upload from "../middlewares/multer";
+
+const router = express.Router();
+
+router.route("/").post(isAuthenticated, upload.single("imageFile"), createRestaurant);
+router.route("/").get(isAuthenticated, getRestaurant);
+router.route("/").put(isAuthenticated, upload.single("imageFile"), updateRestaurant);
+router.route("/order").get(isAuthenticated,  getRestaurantOrder);
+router.route("/order/:orderId/status").put(isAuthenticated, updateOrderStatus);
+router.route("/search/:searchText").get(isAuthenticated, searchRestaurant);
+router.route("/:id").get(isAuthenticated, getSingleRestaurant);
+
+export default router;
+
+
