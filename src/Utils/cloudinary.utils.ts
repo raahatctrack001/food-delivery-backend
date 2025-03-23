d516c068d4af7d82c74a35cc335d04766d3fe423
Raahat Khan <raahatctrack001.rc@gmail.com>
2025-03-23 15:07:59 +0530

    add cloudinary for image upload

---
+++ src/Utils/cloudinary.utils.ts
@@ -0,0 +1,37 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './.env' });
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    if (!localFilePath) return null;
+
+    try {
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto'
+        });
+        console.log(response);
+        return response;
+    } catch (error) {
+        console.error(error);
+        return null;
+    } finally {
+        // Ensure the file is deleted even if an error occurs
+        if (fs.existsSync(localFilePath)) {
+            try {
+                fs.unlinkSync(localFilePath);
+            } catch (unlinkError) {
+                console.error(`Error deleting local file: ${unlinkError}`);
+            }
+        }
+    }
+};
+
+export { uploadOnCloudinary };
